4ad2225b5243ad71538595fc14dc0cd02e524157
Kent <leekent173@gmail.com>
2024-03-20 20:39:31 +0800

    3/20 SDC completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import BookList from "./Component/BookList";
 import Navbar from "./Component/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from "./Component/Layout";
+
+
 function App() {
   return (
     <div>
-      <Navbar/>
-      <BookList/>
+
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<h1>Home</h1>} />
+                <Route path="mybook" element={<BookList />} />
+                <Route path="joinbook" element={<h1>join-book</h1>} />
+                <Route path="login" element={<h1>login</h1>} />
+                <Route path="*" element={<h1>Error 404: Not Found</h1>} />
+            </Route>
+        </Routes>      
+      </BrowserRouter>
+
+      {/* <Navbar/>
+      <BookList/> */}
     </div>
     
   );
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,27 @@
+import logo from '../Assets/logo.png';
+
+
 function Navbar(){
     return(
         <div>
-            <h1>Navbar</h1>
+            <div className="flex justify-between items-center h-18 px-4  text-gray-dark border-b-2 border-gray-dark">
+                <img className="my-1 w-14 h-12" src={logo} alt=""></img>
+                <h1 className='w-full text-5xl font-bold text-[#EE7214]'>BILLBOOK</h1>
+                <ul className="flex whitespace-nowrap item-center">
+                    <li className="mx-4">
+                        <a href='#'>HOME</a>
+                    </li>
+                    <li className="mx-4">
+                        <a href='#'>MY BOOK</a>
+                    </li>
+                    <li className="mx-4">
+                        <a href='#'>JOIN BOOK</a>
+                    </li>
+                    <li>
+                        <a href="">LOGIN</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
