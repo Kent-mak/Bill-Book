ce41a1f2b4cf37f786afc4843f90cc8367a70dae
Kent <leekent173@gmail.com>
2024-03-20 20:21:18 +0800

    3/20 SDC lesson 2

---
+++ src/Component/BookListItem.jsx
@@ -0,0 +1,16 @@
+function BookListItem({BookName='Unknown Name', BookDesc='No Description', BookCover='Empty Book Cover'}){
+    
+    
+    return(
+        <div className="flex flex-col justify-between bg-gray shadow-md rounded-xl overflow-hidden w-72 pt-4 px-0">
+            <img src={BookCover} alt="book cover" className="h-[150px] px-4 mb-4 rounded-t-xl object-cover"/>
+            <div className="px-2 mb-4">
+                <h1 className="text-2xl font-bold mb-2">{BookName}</h1>
+                <p className="text-base">{BookDesc}</p>
+            </div>
+            <a href="#" className="text-lg text-center py-2 font-bold border-t-2 border-gray-dark">Join</a>
+        </div>
+    )
+}
+
+export default BookListItem;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   ],
   
   theme: {
+    colors: {
+      'gray': '#D9D9D9',
+      'gray-light': '#EAEAEA',
+      'gray-dark': '#929191',
+    },
     extend: {},
   },
   plugins: [],
